b9909b8d93cde7f7163200550e25a74d09fe3de7
senilica@gmail.com <senilica@gmail.com>
2013-09-21 12:58:20 +0200

    SDLWrapper.h färdig, även lagt till Makefile (som kommer förbättras och fixas ordentligt senare)

--- C/snake/main.c
+++ C/snake/main.c
@@ -1,8 +1,17 @@
-
-
+#include <SDL.h>
+#include "SDLWrapper.h"
 
 int main(int argc, char* argv[])
 {
-
+  sSdlWrapper* wrap = initializeSDLWrapper("Test", 800, 600, 32, 1, 1);
+  while(isRunning(wrap))
+  {
+    beginFrame(wrap);
+    drawBevel(wrap, 396,296, 8, 8, makeColor(255, 0, 200,200), makeColor(255,200,200,200));
+    if(keyUp(wrap, SDLK_ESCAPE))
+      toggleRunning(wrap);
+    endFrame(wrap);
+  }
+  deinitializeWrapper(wrap);
   return 0;
 }
