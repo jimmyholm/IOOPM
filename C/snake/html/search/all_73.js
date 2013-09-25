81bb7d4f59204835fb6b336814f41a0a110dd386
senilica <senilica@gmail.com>
2013-09-25 15:42:58 +0200

    Linked List documentation written.

---
+++ C/snake/html/search/all_73.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['slinkedlist',['sLinkedList',['../LinkedList_8h.html#a0d43185f5e56c225162709042b7d5fbb',1,'LinkedList.h']]],
+  ['slistiterator',['sListIterator',['../LinkedList_8h.html#adb13ef98c9d5c2c002003dc8510e862b',1,'LinkedList.h']]]
+];
---
+++ C/snake/html/search/functions_6c.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['listclear',['listClear',['../LinkedList_8h.html#adb011b5925686c67a1deb9478ca4dcf2',1,'LinkedList.h']]],
+  ['listempty',['listEmpty',['../LinkedList_8h.html#a6b8d033c1babffbc0749d5feacb49829',1,'LinkedList.h']]],
+  ['listerase',['listErase',['../LinkedList_8h.html#a8746a945dee8f65ba24cefe9c20e5828',1,'LinkedList.h']]],
+  ['listget',['listGet',['../LinkedList_8h.html#a1440a52d37f5aea954fbfd5f668d93bd',1,'LinkedList.h']]],
+  ['listhead',['listHead',['../LinkedList_8h.html#a9c0dc1f5156553695ffe0a967d7a7495',1,'LinkedList.h']]],
+  ['listinitialize',['listInitialize',['../LinkedList_8h.html#a60cfa38a3c6c1b08cc61b3760eba4257',1,'LinkedList.h']]],
+  ['listinsert',['listInsert',['../LinkedList_8h.html#a922d14973f2aa4dd343c3ba90bb725ee',1,'LinkedList.h']]],
+  ['listiteratorend',['listIteratorEnd',['../LinkedList_8h.html#a23aad111319396e7657e582146fed1cf',1,'LinkedList.h']]],
+  ['listiteratornext',['listIteratorNext',['../LinkedList_8h.html#abc46d80bd98af40fac6941bab8399a8b',1,'LinkedList.h']]],
+  ['listpopback',['listPopBack',['../LinkedList_8h.html#a3972ae5a633404280da1a861636d3b25',1,'LinkedList.h']]],
+  ['listpopfront',['listPopFront',['../LinkedList_8h.html#ac3023af5dfd5e99cf4038d72f51ba15f',1,'LinkedList.h']]],
+  ['listpushback',['listPushBack',['../LinkedList_8h.html#adfad82daa71b12c78415d2e82f125093',1,'LinkedList.h']]],
+  ['listpushfront',['listPushFront',['../LinkedList_8h.html#a3e86e16cb6b338c57238ce0e02ecca3f',1,'LinkedList.h']]],
+  ['listsize',['listSize',['../LinkedList_8h.html#a340e12e1be5c1a7e06d0022bdd9960f5',1,'LinkedList.h']]]
+];
