8c9b91df6de22cfb01adec3e9de6dac53fd7dfeb
senilica <senilica@gmail.com>
2013-10-08 15:07:10 +0200

    dice done

--- Java/die/PairOfDice.java
+++ Java/die/PairOfDice.java
@@ -1,22 +1,39 @@
 import java.util.Scanner;
 
 public class PairOfDice {
-    private Die die1;
-    private Die die2;
+  private Die die1;
+  private Die die2;
+  
+  public PairOfDice () {
+    die1 = new Die ();
+    die2 = new Die ();
+  }
+  
+  public PairOfDice (int Sides){
+    die1 = new Die (Sides);
+    die2 = new Die (Sides);
+  }
+  
+  public int Roll (){
+    return (die1.roll() + die2.roll());
+  }
+  
+  public int GetDie1 (){
+    return die1.get();
+  }
+  
+  public int GetDie2 (){
+    return die2.get();
+  }
+  
+  public String toString (){
+    return "PairOfDice (" + die1.get() +"," + die2.get() + ")";
+  }
 
-    public PairOfDice () {
-        die1 = new Die ();
-        die2 = new Die ();
-    }
-
-    public PairOfDice (int Sides){
-        die1 = new Die (Sides);
-        die2 = new Die (Sides);
-    }
-
-    public int RollPairOfDies (){
-        return (die1.roll() + die2.roll());
-    }
-
-    public int GetDie1 (){
-retur
+  public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Number of sides ");
+    PairOfDice pd = new PairOfDice(sc.nextInt());
+    System.out.println("Alea iacta est " + pd.Roll() +pd);
+  }
+}
